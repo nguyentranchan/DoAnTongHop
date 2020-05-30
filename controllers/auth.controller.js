c6bef78996b562fdc81a2340c18ad587ff5ad69b
nguyentranchan <nguyentranchan1998@gmail.com>
2020-05-31 00:09:01 +0700

    'edit_auth_controler'

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,41 +9,41 @@ module.exports.login = function (req,res){
 }
 
 module.exports.postlogin = async function (req, res, next){
-	const email = req.body.email;
-	const password = req.body.password;
-	var haspassword = md5(password);
-    var user = await User.findOne({email: email});
+    const email = req.body.email
+    const password = req.body.password 
+    var haspassword = md5(password)
+    var user = await User.findOne({email: email})
 	if(!user){
 		res.render('admin/login',{
 			errors : ['User does not exit.']
 		});
-		return;
+		return
 	}else{
         if (user.password !== haspassword) {
             // statement
             res.render('admin/login',{
                 errors : ['Password not true.']
-            });
-            return;
+            })
+            return
         }else{
             res.cookie('userEmail',user.email,{signed: true});
             res.redirect('/admin')
         }       
     }
-    next();
+    next()
 }
 
 module.exports.resiger = function(req, res) {
     if(req.signedCookies.userEmail){
-        res.redirect('/admin');
+        res.redirect('/admin')
     }else
-        res.render('admin/resiger');
+        res.render('admin/resiger')
 }
 
 module.exports.postRes = async function( req, res) {
-    req.body.password = md5(req.body.password);
-    const user = await User.insertMany(req.body);
-    res.redirect('/auth');
+    req.body.password = md5(req.body.password)
+    const user = await User.insertMany(req.body)
+    res.redirect('/auth')
 }
 
 
